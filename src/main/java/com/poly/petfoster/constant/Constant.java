4a25aa22add0f72bedb760a69782820a0a6280e0
nkhangg <phamnhutkhanggpc04910@gmail.com>
2023-11-15 10:45:48 +0700

    rewrite-return-url

--- src/main/java/com/poly/petfoster/constant/Constant.java
+++ src/main/java/com/poly/petfoster/constant/Constant.java
@@ -11,7 +11,7 @@ public class Constant {
 
     // vn pay
     public static String VNP_PAY_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String VNP_RETURN_URL = "http://localhost:8080/vnpay_jsp/vnpay_return.jsp";
+    public static String VNP_RETURN_URL = "http://localhost:3000/payment";
     public static String VNP_TMN_CODE = "EWMX8UAJ";
     public static String VNP_SECRET_KEY = "YGFMMZSPJSYYKBXRJPBPOSOEWZUEGKPF";
     public static String VNP_API_ARL = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
