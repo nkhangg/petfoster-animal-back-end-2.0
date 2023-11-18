80af0fd50cd9fefef1328a4383eb67e6cf0d860c
Duy220807 <127750881+Duy220807@users.noreply.github.com>
2023-11-19 06:29:05 +0700

    duynq/update-filter-api-get-all-user

--- src/main/java/com/poly/petfoster/repository/UserRepository.java
+++ src/main/java/com/poly/petfoster/repository/UserRepository.java
@@ -11,26 +11,41 @@ import com.poly.petfoster.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
-    
-    Boolean existsByUsername(String username);
+        Boolean existsByUsername(String username);
 
-    Boolean existsByEmail(String email);
+        Boolean existsByEmail(String email);
 
-    Boolean existsByPhone(String phone);
+        Boolean existsByPhone(String phone);
 
-    @Query("select p from Users p where p.isActive = true")
-    public List<User> findAll();
+        @Query("select p from Users p where p.isActive = true")
+        public List<User> findAll();
 
-    @Query("select u from Users u where u.email = :email")
-    public Optional<User> findByEmail(@Param("email") String email);
+        @Query("select u from Users u where u.email = :email")
+        public Optional<User> findByEmail(@Param("email") String email);
 
-    @Query("select u from Users u where u.username = :username")
-    public Optional<User> findByUsername(@Param("username") String username);
+        @Query("select u from Users u where u.username = :username")
+        public Optional<User> findByUsername(@Param("username") String username);
 
-    @Query(nativeQuery = true, value = "select count(*) from users where is_active = 'true'")
-    public Integer getTotalUsers();
+        @Query(nativeQuery = true, value = "select count(*) from users where is_active = 'true'")
+        public Integer getTotalUsers();
 
-    @Query(nativeQuery = true, value = "select * from users where token = :token")
-    public User findByToken(@Param("token") String token);
+        @Query(nativeQuery = true, value = "select * from users where token = :token")
+        public User findByToken(@Param("token") String token);
+
+        @Query(nativeQuery = true, value = "SELECT u.* FROM users u INNER JOIN authorities a ON u.[user_id] = a.[user_id] "
+                        + "INNER JOIN [role] r ON r.id = a.role_id " +
+                        "WHERE (username IS NULL OR u.username LIKE %:username% ) " +
+                        "AND (fullname IS NULL OR u.fullname LIKE %:fullname% ) " +
+                        "AND (email IS NULL OR u.email LIKE %:email%) " +
+                        "AND (gender IS NULL OR u.gender LIKE %:gender%) " +
+                        "AND (phone IS NULL OR u.phone LIKE %:phone%) " +
+                        "AND (r.[role] IS NULL OR r.[role] LIKE %:role%) ")
+        List<User> filterUsers(
+                        @Param("username") String username,
+                        @Param("fullname") String fullname,
+                        @Param("email") String email,
+                        @Param("gender") String gender,
+                        @Param("phone") String phone,
+                        @Param("role") String role);
 
 }
