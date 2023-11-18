fb90f9429e86bae37b14d87eb72b40365f7901b7
trungduc102 <trungduc102vn@gmail.com>
2023-11-18 22:48:40 +0700

    Co them get All Brand

--- src/main/java/com/poly/petfoster/repository/BrandRepository.java
+++ src/main/java/com/poly/petfoster/repository/BrandRepository.java
@@ -1,9 +1,17 @@
 package com.poly.petfoster.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.poly.petfoster.entity.Brand;
+import com.poly.petfoster.entity.Review;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
-    
+    @Query( nativeQuery=true, value="select* from brand where brand= :name")
+    public Optional<List<Brand>> findbyName(@Param("name") String name);
+
 }
