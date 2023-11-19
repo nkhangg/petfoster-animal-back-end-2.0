de861b91cde218d69b674bcf039fd47adb700ebc
LamHaNguyen <qianqian1810@gmail.com>
2023-11-19 23:06:54 +0700

    api-admin-order-details

--- src/main/java/com/poly/petfoster/controller/admin/orders/OrderFilterController.java
+++ src/main/java/com/poly/petfoster/controller/admin/orders/OrderFilterController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +32,11 @@ public class OrderFilterController {
             @RequestParam("maxDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Optional<Date> maxDate,
             @RequestParam("sort") Optional<String> sort) {
         return ResponseEntity.ok(orderFilterService.filterOrders(username, orderId, status, minDate, maxDate, sort));
+    }
+
+    @GetMapping("/details/{id}")
+    public ResponseEntity<ApiResponse> orderDetails(@PathVariable Integer id) {
+        return ResponseEntity.ok(orderFilterService.orderDetails(id));
     }
 }
 
--- src/main/java/com/poly/petfoster/service/admin/order/OrderFilterService.java
+++ src/main/java/com/poly/petfoster/service/admin/order/OrderFilterService.java
@@ -8,5 +8,7 @@ import com.poly.petfoster.response.ApiResponse;
 public interface OrderFilterService {
 
     ApiResponse filterOrders(Optional<String> username, Optional<Integer> orderId, Optional<String> status, Optional<Date> minDate, Optional<Date> maxDate, Optional<String> sort);
+
+    ApiResponse orderDetails(Integer id);
     
 }
