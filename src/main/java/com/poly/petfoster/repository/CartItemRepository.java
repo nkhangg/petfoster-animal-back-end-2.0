2bbb9a0259cdb27cd7abd271d10c1b0c383c9b31
nkhangg <phamnhutkhanggpc04910@gmail.com>
2023-11-20 01:07:56 +0700

    fix-carts-user

--- src/main/java/com/poly/petfoster/repository/CartItemRepository.java
+++ src/main/java/com/poly/petfoster/repository/CartItemRepository.java
@@ -1,6 +1,7 @@
 package com.poly.petfoster.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +11,9 @@ import com.poly.petfoster.entity.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
     @Query(nativeQuery = true, value = "select * from cart_item where cart_id = :cartId")
-    List<CartItem> cartItem(@Param("cartId") Integer cartId);
+    List<CartItem> findByCartsId(@Param("cartId") Integer cartId);
+
+    @Query("select ci from CartItem ci where ci.productRepo.product.id = :productId and ci.productRepo.size = :size and ci.cart.id = :cartId")
+    Optional<CartItem> findBySizeAndProductId(@Param("cartId") Integer cartId, @Param("productId") String productId,
+            @Param("size") int size);
 }
