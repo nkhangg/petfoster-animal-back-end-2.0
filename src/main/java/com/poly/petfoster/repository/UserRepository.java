25f829c7d1b02021aec611f54067d2888f0659c7
Duy220807 <127750881+Duy220807@users.noreply.github.com>
2023-11-21 05:42:13 +0700

    Revert "duynq/update-filter-api-get-all-user"

--- src/main/java/com/poly/petfoster/repository/UserRepository.java
+++ src/main/java/com/poly/petfoster/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.poly.petfoster.repository;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,37 +11,26 @@ import com.poly.petfoster.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
-        Boolean existsByUsername(String username);
+    
+    Boolean existsByUsername(String username);
 
-        Boolean existsByEmail(String email);
+    Boolean existsByEmail(String email);
 
-        Boolean existsByPhone(String phone);
+    Boolean existsByPhone(String phone);
 
-        @Query("SELECT u FROM Users u WHERE u.isActive = 1 " +
-                        "AND (:keyword IS NULL OR u.username LIKE %:keyword% ) " +
-                        "OR (:keyword IS NULL OR u.fullname LIKE %:keyword% ) " +
-                        "OR (:keyword IS NULL OR u.email LIKE %:keyword%) " +
-                        "ORDER BY " +
-                        "CASE WHEN :sort = 'username-desc' THEN u.username END DESC, " +
-                        "CASE WHEN :sort = 'username-asc' THEN u.username END ASC, " +
-                        "CASE WHEN :sort = 'fullname-desc' THEN u.fullname END DESC, " +
-                        "CASE WHEN :sort = 'fullname-asc' THEN u.fullname END ASC, " +
-                        "CASE WHEN :sort = 'create-desc' THEN u.createAt END DESC, " +
-                        "CASE WHEN :sort = 'create-asc' THEN u.createAt END ASC, " +
-                        "CASE WHEN :sort = 'birthday-desc' THEN u.birthday END DESC, " +
-                        "CASE WHEN :sort = 'birthday-asc' THEN u.birthday END ASC")
-        public List<User> findAll(@Param("keyword") String keyword,
-                        @Param("sort") String sort);
+    @Query("select p from Users p where p.isActive = true")
+    public List<User> findAll();
 
-        @Query("select u from Users u where u.email = :email")
-        public Optional<User> findByEmail(@Param("email") String email);
+    @Query("select u from Users u where u.email = :email")
+    public Optional<User> findByEmail(@Param("email") String email);
 
-        @Query("select u from Users u where u.username = :username")
-        public Optional<User> findByUsername(@Param("username") String username);
+    @Query("select u from Users u where u.username = :username")
+    public Optional<User> findByUsername(@Param("username") String username);
 
-        @Query(nativeQuery = true, value = "select count(*) from users where is_active = 'true'")
-        public Integer getTotalUsers();
+    @Query(nativeQuery = true, value = "select count(*) from users where is_active = 'true'")
+    public Integer getTotalUsers();
 
-        @Query(nativeQuery = true, value = "select * from users where token = :token")
-        public User findByToken(@Param("token") String token);
+    @Query(nativeQuery = true, value = "select * from users where token = :token")
+    public User findByToken(@Param("token") String token);
+
 }
--- src/main/java/com/poly/petfoster/service/impl/user/UserServiceImpl.java
+++ src/main/java/com/poly/petfoster/service/impl/user/UserServiceImpl.java
@@ -111,20 +111,9 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public ApiResponse getAllUser(String jwt, Optional<String> keyword,
-            Optional<String> sort,
-            Optional<Integer> pages) {
+    public ApiResponse getAllUser(String jwt, Optional<Integer> pages) {
 
-        List<User> users = userRepository.findAll(keyword.orElse(null), sort.orElse(null));
-
-        if (users.isEmpty()) {
-            return ApiResponse.builder().message("No data!")
-                    .status(400)
-                    .errors(false)
-                    .data(PagiantionResponse.builder().data(null)
-                            .build())
-                    .build();
-        }
+        List<User> users = userRepository.findAll();
 
         Pageable pageable = PageRequest.of(pages.orElse(0), 10);
         int startIndex = (int) pageable.getOffset();
@@ -145,22 +134,17 @@ public class UserServiceImpl implements UserService {
 
         visibleUsers.forEach((user) -> {
             // find role by user
-            List<Authorities> roles = authoritiesRepository.findByUser(user);
-            Role role = null;
-            if (roles.size() > 0) {
-                role = roles.get(0).getRole();
-            }
-
+            Role role = authoritiesRepository.findByUser(user).get(0).getRole();
             UserProfileResponse userProfile = UserProfileResponse.builder()
                     .id(user.getId())
                     .username(user.getUsername())
                     .fullname(user.getFullname())
                     .birthday(user.getBirthday())
-                    .gender(user.getGender() == null ? false : true)
+                    .gender(user.getGender())
                     .phone(user.getPhone())
                     .email(user.getEmail())
                     .avatar(user.getAvatar() == null ? null : portUltil.getUrlImage(user.getAvatar()))
-                    .role(role == null ? null : role.getRole())
+                    .role(role.getRole())
                     .createAt(user.getCreateAt())
                     .build();
 
