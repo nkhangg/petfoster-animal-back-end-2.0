25506fb3422ceafe6d1c4bf90f0decd7b3283c8c
nkhangg <phamnhutkhanggpc04910@gmail.com>
2024-01-23 00:56:22 +0700

    payload-adopt

---
+++ src/main/java/com/poly/petfoster/response/adopts/AdoptsResponse.java
@@ -0,0 +1,24 @@
+package com.poly.petfoster.response.adopts;
+
+import java.util.Date;
+
+import com.poly.petfoster.response.pets.PetResponse;
+import com.poly.petfoster.response.users.UserProfileResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdoptsResponse {
+    private Integer id;
+    private String state;
+    private UserProfileResponse user;
+    private PetResponse pet;
+    private Date registerAt;
+    private Date adoptAt; // can null if watting or cancel
+}
