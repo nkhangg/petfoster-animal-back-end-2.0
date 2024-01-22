88acd560d8b7b7dd731871b60850b24071a2605e
Duy220807 <127750881+Duy220807@users.noreply.github.com>
2024-01-23 00:03:37 +0700

    update feedback
    add pagination

--- src/main/java/com/poly/petfoster/controller/admin/feedback/ManagementFeedbackController.java
+++ src/main/java/com/poly/petfoster/controller/admin/feedback/ManagementFeedbackController.java
@@ -6,20 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poly.petfoster.response.ApiResponse;
 import com.poly.petfoster.service.feedback.FeedbackService;
 
 @RestController
-@RequestMapping("/api/admin/feedbacks")
+@RequestMapping("/api/test/feedbacks")
 public class ManagementFeedbackController {
     @Autowired
     FeedbackService feedbackService;
 
     @GetMapping("")
-    public ResponseEntity<ApiResponse> getFeedback() {
-        return ResponseEntity.ok(feedbackService.getFeedback());
+    public ResponseEntity<ApiResponse> getFeedback(@RequestParam("page") int page) {
+        return ResponseEntity.ok(feedbackService.getFeedback(page));
     }
 
     @PutMapping("/{id}")
